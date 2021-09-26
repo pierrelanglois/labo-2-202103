c1d5fc4a57da9ae7e4ab4b850ce69a1f1159ee35
Pierre Langlois <pierre.langlois@polymtl.ca>
2021-09-26 17:18:33 -0400

    Update vote_majorite_qualifiee_TB.vhd
    Correction de l'alignement, remplacer 'tab' par espaces.

--- sources/vote_majorite_qualifiee_TB.vhd
+++ sources/vote_majorite_qualifiee_TB.vhd
@@ -17,18 +17,18 @@ entity vote_majorite_qualifiee_TB is
         N_juges : positive := 3;
         N_senateurs : positive := 4;
         N_deputes : positive := 5
-	);
+    );
 end vote_majorite_qualifiee_TB;
 
 architecture arch of vote_majorite_qualifiee_TB is
-	
+    
 signal votes_juges : std_logic_vector(N_juges - 1 downto 0);
 signal votes_senateurs : std_logic_vector(N_senateurs - 1 downto 0);
 signal votes_deputes : std_logic_vector(N_deputes - 1 downto 0);
 signal approbation : std_logic;
 
 function verification_approbation(votes_juges, votes_senateurs, votes_deputes : std_logic_vector) return boolean is
-begin	
+begin    
     
 --
 -- code à compléter
@@ -41,25 +41,25 @@ end verification_approbation;
 
 begin
 
-	-- instanciation du module à vérifier à l'aide d'une instantiation d'entité (VHDL LRM 2002, section 9.6.2)
+    -- instanciation du module à vérifier à l'aide d'une instantiation d'entité (VHDL LRM 2002, section 9.6.2)
     UUT : entity vote_majorite_qualifiee(arch_equations_345)
 --    UUT : entity vote_majorite_qualifiee(arch_flot_donnees_456)
 --    UUT : entity vote_majorite_qualifiee(arch_comportementale)
         generic map (N_juges, N_senateurs, N_deputes)
         port map (votes_juges, votes_senateurs, votes_deputes, approbation);
-	
-	process -- application exhaustive des vecteurs de test
-	begin
-		for i in 0 to 2 ** N_juges - 1 loop
-			votes_juges <= std_logic_vector(to_unsigned(i, N_juges));
-    		for j in 0 to 2 ** N_senateurs - 1 loop
-    			votes_senateurs <= std_logic_vector(to_unsigned(j, N_senateurs));
-        		for k in 0 to 2 ** N_deputes - 1 loop
-        			votes_deputes <= std_logic_vector(to_unsigned(k, N_deputes));
+    
+    process -- application exhaustive des vecteurs de test
+    begin
+        for i in 0 to 2 ** N_juges - 1 loop
+            votes_juges <= std_logic_vector(to_unsigned(i, N_juges));
+            for j in 0 to 2 ** N_senateurs - 1 loop
+                votes_senateurs <= std_logic_vector(to_unsigned(j, N_senateurs));
+                for k in 0 to 2 ** N_deputes - 1 loop
+                    votes_deputes <= std_logic_vector(to_unsigned(k, N_deputes));
                     wait for 10 ns;
                     
                     assert (approbation = '1') = verification_approbation(votes_juges, votes_senateurs, votes_deputes)
-    				report "Erreur ! "
+                        report "Erreur ! "
                         & "votes_juges : " & to_string(votes_juges) & ", "
                         & "votes_senateurs : " & to_string(votes_senateurs) & ", "
                         & "votes_deputes : " & to_string(votes_deputes) & ", "
@@ -70,8 +70,8 @@ begin
             end loop;
         end loop;
 
-		report "simulation terminée" severity failure;
-		
-	end process;
-	
+        report "simulation terminée" severity failure;
+        
+    end process;
+    
 end;
